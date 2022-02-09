3939700f37b2f6d3c6f207462b52d5dea48c6c51
pdhakad <pdhakad@AZ-002>
2022-02-09 10:07:03 +0530

    Add project files.

---
+++ WebApplicationCoreMVC/Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebApplicationCoreMVC.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Studentadd()
+        {
+            return View();
+        }
+    }
+}
